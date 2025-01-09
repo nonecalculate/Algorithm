d9c791c3bf9e41525256a2f1d2e0a75497150824
nonecalculate <127222937+nonecalculate@users.noreply.github.com>
2025-01-09 16:58:03 +0900

    Update CHAPTHER3.cpp

--- 과제/5번과제/CHAPTHER3.cpp
+++ 과제/5번과제/CHAPTHER3.cpp
@@ -1,19 +1,18 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "MyActor2.h"
+#include "MyActor.h"
 #include "Math/UnrealMathUtility.h"
 #include "Containers/Array.h"
 #include "Engine/Engine.h"
 
-AMyActor2::AMyActor2()
+AMyActor::AMyActor()
 {
 	PrimaryActorTick.bCanEverTick = true;
 	start = FVector2D(0, 0);
 	eventCount = 0;
 	totalDist = 0;
+
 }
 
-void AMyActor2::BeginPlay()
+void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
 	move();
@@ -21,49 +20,45 @@ void AMyActor2::BeginPlay()
 	UE_LOG(LogTemp, Warning, TEXT("총 이동 발생 횟수는 : %f 입니다"), eventCount);
 }
 
-
-int32 AMyActor2::step() 
+int32 AMyActor::step()
 {
 	return FMath::RandRange(0, 1);
 }
 
-float AMyActor2::Dist(FVector2D first, FVector2D second)
+float AMyActor::Dist(FVector2D f, FVector2D s)
 {
-	float dx = first.X - second.X;
-	float dy = first.Y - second.Y;
+	float dx = f.X - s.X;
+	float dy = f.Y - s.Y;
 	return FMath::Sqrt(dx * dx + dy * dy);
 }
 
-int32 AMyActor2::createEvent() {
+int32 AMyActor::createEvent() {
 	int32 r = FMath::RandRange(1, 2);
-	if (r == 1) 
+	if (r == 1)
 	{
 		UE_LOG(LogTemp, Log, TEXT("이벤트가 발생했습니다!"));
 		return 1;
 	}
-	else 
+	else
 	{
 		UE_LOG(LogTemp, Log, TEXT("아직은 잠잠합니다..."));
 		return 0;
 	}
 }
 
-void AMyActor2::move()
+void AMyActor::move()
 {
 	FVector2D crt = start;
 	FVector2D prv = crt;
-	for (int32 i = 0; i < 10; i++) 
+	for (int32 i = 0; i < 10; i++)
 	{
 		FVector2D next(crt.X + step(), crt.Y + step());
-
 		if (next.X - crt.X <= 2 && next.Y - crt.Y <= 2)
 		{
 			float dist = Dist(crt, next);
 			totalDist += dist;
 			UE_LOG(LogTemp, Warning, TEXT("%d번째 스텝입니다. 현재 좌표는 (%f,%f)이며 이동 거리는: %f 입니다."), i + 1, next.X, next.Y, dist);
-
 			eventCount += createEvent();
-
 			prv = crt;
 			crt = next;
 		}
@@ -74,3 +69,4 @@ void AMyActor2::move()
 	}
 
 }
+
