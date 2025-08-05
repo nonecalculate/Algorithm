abf0a7bebe6a56f65d43cb0cb5dfd1b691658243
nonecalculate <escortbayonet@gmail.com>
2025-08-05 15:56:12 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 700 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+vector<int> arr;
+int N = 0;
+
+int main()
+{
+    cin >> N;
+    arr.resize(N);
+    for (int i = 0; i < N; i++)cin >> arr[i];
+
+    sort(arr.begin(),arr.end());
+
+    for (int i = 0; i < N; i++)cout << arr[i] << "\n";
+    return 0;
+}
